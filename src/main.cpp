4ecebd0b49e84647e5a4d4d2c594ce8c7141365a
Jernej Kos <k@jst.sm>
2009-09-13 12:04:24 +0000

    Added basic backend interface (probably still missing some stuff).

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,17 @@
 #include <QtGui/QApplication>
 #include "gui/wizard.h"
 
+/**
+ * Program entry point.
+ */
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    FlashWizard w;
-    w.show();
-    return a.exec();
+  QApplication a(argc, argv);
+  
+  // Flash wizard is our main widget that goes through the whole
+  // firmware flashing process.
+  FlashWizard w;
+  w.show();
+  
+  return a.exec();
 }
